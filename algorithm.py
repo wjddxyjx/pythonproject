1ca194f4eed3f7ffe983aca7e84fb22e463bff60
杨嘉欣 <wjddxyjx@163.com>
2020-09-11 19:58:15 +0800

    增加了报错提示框

--- algorithm.py
+++ algorithm.py
@@ -3,6 +3,10 @@ import re
 import pandas as pd
 import numpy as np
 import os
+
+from PyQt5.QtWidgets import QMessageBox
+
+
 class read_excelfile:
     #初始化类
     #类成员定义：
@@ -20,13 +24,19 @@ class read_excelfile:
     def readmulti(self):
 
         for num in range(0, self.num):
-            df1 = pd.read_excel(self.var[num], sheet_name = self.sheetvar,header=None)
-            self.a.append(np.array(df1))
-            for row1 in range(0, self.a[num].shape[0]):
-                for column in range(0, self.a[num].shape[1]):
-                    if (isinstance(self.a[num][row1, column], str)):
-                        self.a[num][row1, column] = re.sub(r'[^\w\s]', '', self.a[num][row1, column])
-                        self.a[num][row1, column] = "".join(self.a[num][row1, column].split())
+            try:
+                df1 = pd.read_excel(self.var[num], sheet_name = self.sheetvar,header=None)
+                self.a.append(np.array(df1))
+            except FileNotFoundError:
+                QMessageBox.warning(None, '警告', '请检查excel文件是否齐全！')
+                return -1
+            else:
+                for row1 in range(0, self.a[num].shape[0]):
+                    for column in range(0, self.a[num].shape[1]):
+                        if (isinstance(self.a[num][row1, column], str)):
+                            self.a[num][row1, column] = re.sub(r'[^\w\s]', '', self.a[num][row1, column])
+                            self.a[num][row1, column] = "".join(self.a[num][row1, column].split())
+        return 0
 
     #计算平均负债率
     def loadrate(self):
@@ -44,3 +54,13 @@ class read_excelfile:
             ration = ration + debt/asset
         ration = ration/self.num
         return ration
+
+    def absolute1(self,var):
+        asset = []
+        for num in range(0, self.num):
+            itemindex = np.argwhere(self.a[num] == var)
+            b = itemindex[0][1] + 1
+            c = itemindex[0][0]
+            asset.append(var + str(round(self.a[num][c, b],2)))
+        return asset
+
--- process.py
+++ process.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from PyQt5.QtWidgets import QFileDialog, QDialog, QApplication
+from PyQt5.QtWidgets import QFileDialog, QDialog, QApplication, QMessageBox
 from PyQt5.uic.properties import QtWidgets
 
 from calculate import Ui_MainWindow
@@ -30,50 +30,129 @@ class MyPyQT_Form(Ui_MainWindow):
         end_year = int(self.comboBox_5.currentText())
         end_month = int(self.comboBox_4.currentText())
         self.filename = []
+        self.data = []
         year_cha = end_year - start_year
         year_add = 0
         #分两种情况读数
         #1.起始年都在一年内
         if(start_year == end_year):
-            for num in range(start_month,end_month+1):
-                if(num < 10):
-                    self.filename.append(os.getcwd() + '\excel\财务报表' + str(start_year) + str(0) + str(num) + '.xlsx')
-                else:
-                    self.filename.append(os.getcwd() + '\excel\财务报表' + str(start_year) + str(num) + '.xlsx')
+            if(start_month <= end_month):
+                for num in range(start_month,end_month+1):
+                    if(num < 10):
+                        self.filename.append(os.getcwd() + '\excel\财务报表' + str(start_year) + str(0) + str(num) + '.xlsx')
+                        self.data.append(str(start_year)+'年'+str(num)+'月:')
+                    else:
+                        self.filename.append(os.getcwd() + '\excel\财务报表' + str(start_year) + str(num) + '.xlsx')
+                        self.data.append(str(start_year) + '年' + str(num) + '月:')
+            else:
+                QMessageBox.warning(None, '警告', '请确认好起始年月份是否有误！')
+                return
         else:
         # 2.起始年不在一年内
-            for num in range(start_month, 13):
-                if(num < 10):
-                    self.filename.append(os.getcwd() + '\excel\财务报表' + str(start_year) + str(0) + str(num) + '.xlsx')
-                else:
-                    self.filename.append(os.getcwd() + '\excel\财务报表' + str(start_year) + str(num) + '.xlsx')
-
-            while(year_cha > 1):
-                year_add = year_add + 1
-                for num in range(1, 13):
+            if (start_year < end_year):
+                for num in range(start_month, 13):
                     if(num < 10):
-                        self.filename.append(os.getcwd() + '\excel\财务报表' + str(start_year + year_add) + str(0) + str(num) + '.xlsx')
+                        self.filename.append(os.getcwd() + '\excel\财务报表' + str(start_year) + str(0) + str(num) + '.xlsx')
+                        self.data.append(str(start_year) + '年' + str(num) + '月:')
                     else:
-                        self.filename.append(os.getcwd() + '\excel\财务报表' + str(start_year + year_add) + str(num) + '.xlsx')
-                year_cha = year_cha - 1
+                        self.filename.append(os.getcwd() + '\excel\财务报表' + str(start_year) + str(num) + '.xlsx')
+                        self.data.append(str(start_year) + '年' + str(num) + '月:')
 
-            for num in range(1, end_month+1):
-                if(num < 10):
-                    self.filename.append(os.getcwd() + '\excel\财务报表' + str(end_year) + str(0) + str(num) + '.xlsx')
-                else:
-                    self.filename.append(os.getcwd() + '\excel\财务报表' + str(end_year) + str(num) + '.xlsx')
-        #根据下拉框索引选择不同的处理方式
-        result = self.switch_case(self.comboBox.currentIndex())
-        self.textBrowser.append(str(result))
+                while(year_cha > 1):
+                    year_add = year_add + 1
+                    for num in range(1, 13):
+                        if(num < 10):
+                            self.filename.append(os.getcwd() + '\excel\财务报表' + str(start_year + year_add) + str(0) + str(num) + '.xlsx')
+                            self.data.append(str(start_year + year_add) + '年' + str(num) + '月:')
+                        else:
+                            self.filename.append(os.getcwd() + '\excel\财务报表' + str(start_year + year_add) + str(num) + '.xlsx')
+                            self.data.append(str(start_year + year_add) + '年' + str(num) + '月:')
+                    year_cha = year_cha - 1
 
+                for num in range(1, end_month+1):
+                    if(num < 10):
+                        self.filename.append(os.getcwd() + '\excel\财务报表' + str(end_year) + str(0) + str(num) + '.xlsx')
+                        self.data.append(str(end_year) + '年' + str(num) + '月:')
+                    else:
+                        self.filename.append(os.getcwd() + '\excel\财务报表' + str(end_year) + str(num) + '.xlsx')
+                        self.data.append(str(end_year) + '年' + str(num) + '月:')
+            else:
+                QMessageBox.warning(None, '警告', '请确认好起始年月份是否有误！')
+                return
+            #根据不同页数选择不同的处理方式
+        tabindex = self.tabWidget.currentIndex()
+        result = self.switch_case(tabindex)
+        if(result == -1):
+            return
+        else:
+            linenum = len(result)
+            for num in range(0, linenum):
+                self.textBrowser.append((self.data[num]))
+                self.textBrowser.append((result[num]))
 
         #self.excelfile.readmulti()
 
     def switch_case(self,value):
-        switcher = {
-            0: self.loadingrate()
-        }
-        return switcher.get(value, 'wrong value')
+       if(value == 0):
+        return self.multistats()
+       elif(value == 1):
+        return self.parentstats()
+
+
+    #勾选不同的指标
+    def multistats(self):
+        result = 0
+        if(self.radioButton_2.isChecked()):
+            numindex = self.comboBox_6.currentIndex()
+            result = self.switch_index(numindex)
+        elif(self.radioButton_3.isChecked()):
+            numindex = self.comboBox_7.currentIndex()
+            result = self.switch_index(numindex)
+        else:
+            QMessageBox.warning(None,'警告','请勾选要计算的指标！')
+            result = -1
+        return result
+
+    def parentstats(self):
+        result = 0
+        if (self.radioButton_4.isChecked()):
+            numindex = self.comboBox_6.currentIndex()
+            result = self.switch_index(numindex)
+        elif (self.radioButton_5.isChecked()):
+            numindex = self.comboBox_7.currentIndex()
+            result = self.switch_index(numindex)
+        else:
+
+            QMessageBox.warning(None, '警告', '请勾选要计算的指标！')
+            result = -1
+        return result
+
+    def switch_index(self,value):
+        tabindex = self.tabWidget.currentIndex()
+        if(tabindex == 0):
+            excelfile = read_excelfile(self.filename, '合并资产负债表')
+            fid = excelfile.readmulti()
+            if(fid == -1):
+                return -1
+            else:
+                return self.switch_textindex(excelfile, value)
+        elif(tabindex == 1):
+            excelfile = read_excelfile(self.filename, '母公司资产负债表')
+            fid = excelfile.readmulti()
+            if(fid == -1):
+                return -1
+            else:
+                return self.switch_textindex(excelfile, value)
+
+    def switch_textindex(self,read_excelfile ,value):
+        if (value == 0):
+            return read_excelfile.absolute1('资产总计')
+        elif (value == 1):
+            return read_excelfile.absolute1('负债合计')
+        elif (value == 2):
+            return read_excelfile.absolute('')
+        elif (value == 3):
+            return read_excelfile.absolute('')
 
     #负债率计算，返回值为平均负债率
     def loadingrate(self):
